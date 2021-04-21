975022459cbda462724391e7930fc6c9c6772509
Laabidi-Wafa <laabidiwafa199@gmail.com>
2021-04-21 17:56:54 +0100

    fixing the add User form

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,10 @@
 export class User {
-    id?: any;
-    firstname?: string;
-    lastname?: string;
-    address?: string;
-    phone?: string;
-    date_of_birth?: string;
-    email?: string;
-    password?: string;
-  }
+  id?: any;
+  firstname?: string;
+  lastname?: string;
+  address?: string;
+  phone?: string;
+  date?: string;
+  email?: string;
+  password?: string;
+}
--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -1,27 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { UserserviceService } from '../services/userservice.service';
-import { ActivatedRoute,Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
-  styleUrls: ['./userform.component.css']
+  styleUrls: ['./userform.component.css'],
 })
 export class UserFormComponent implements OnInit {
-
   user: User = {
     firstname: '',
     lastname: '',
     address: '',
     phone: '',
-    date_of_birth: '',
+    date: '',
     email: '',
-    password: ''
+    password: '',
   };
   message = '';
 
-  constructor(private userService: UserserviceService,private router:Router,private route: ActivatedRoute) { }
+  constructor(
+    private userService: UserserviceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.message = '';
@@ -32,50 +35,48 @@ export class UserFormComponent implements OnInit {
   }
 
   editUser(id: string): void {
-    this.userService.get(id)
-      .subscribe(
-        data => {
-          this.user = data;
-        },
-        error => {
-          console.error(error);
-        });
+    this.userService.get(id).subscribe(
+      (data) => {
+        this.user = data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
   saveUser(): void {
     this.message = '';
     if (this.user.id) {
       this.saveEditedUser();
-    }
-    else{
+    } else {
       this.createNewUser();
-    } 
+    }
   }
 
   private createNewUser() {
-    this.userService.create(this.user)
-      .subscribe(
-        response => {
-          this.router.navigate([ '/listuser' ]);
-          alert("User added successfully !!");
-        },
-        error => {
-          console.error(error);
-          this.message = 'An error occurred while saving user';
-        });
+    this.userService.create(this.user).subscribe(
+      (response) => {
+        this.router.navigate(['/listuser']);
+        alert('User added successfully !!');
+      },
+      (error) => {
+        console.error(error);
+        this.message = 'An error occurred while saving user';
+      }
+    );
   }
 
   private saveEditedUser() {
-    this.userService.update(this.user.id, this.user)
-      .subscribe(
-        response => {
-          this.router.navigate([ '/listuser' ]);
-          alert("User updated successfully !!");
-        },
-        error => {
-          console.error(error);
-          this.message = 'An error occurred while saving user';
-        });
+    this.userService.update(this.user.id, this.user).subscribe(
+      (response) => {
+        this.router.navigate(['/listuser']);
+        alert('User updated successfully !!');
+      },
+      (error) => {
+        console.error(error);
+        this.message = 'An error occurred while saving user';
+      }
+    );
   }
-
 }
